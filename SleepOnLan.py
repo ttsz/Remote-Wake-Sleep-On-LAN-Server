574b793c4951942bc2732a10442b3703a8c8c6ca
ttsz <ttsz84@gmail.com>
2016-07-17 21:48:01 +0200

    Remove unused line into SleepOnLine

--- SleepOnLan.py
+++ SleepOnLan.py
@@ -17,7 +17,6 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.send_header("Content-type", "text/html")
         s.end_headers()
         s.wfile.write("<html><head><title>Title goes here.</title></head>")
-        s.wfile.write("<body><p>This is a test.</p>")
         # If someone went to "http://something.somewhere.net/foo/bar/",
         # then s.path equals "/foo/bar/".
         s.wfile.write("<p>You accessed path: %s</p>" % s.path)
